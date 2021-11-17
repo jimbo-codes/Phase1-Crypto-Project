560c3ad10693a21e2b146e66e27e094a37355394
Prajay Tipre <prajaytipre@gmail.com>
2021-11-17 16:08:29 -0500

    First working build

--- public/index.js
+++ public/index.js
@@ -1,20 +1,30 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector('button[type="submit"]').addEventListener('click', (e) => {
-        e.preventDefault();
-        let inputVal = document.querySelector('#searchBar').value;
-        let searchForm = document.getElementById('searchForm')
-        fetch(`https://api.coingecko.com/api/v3/coins/${inputVal}`)
-        .then(resp => resp.json())
-        .then(data => {
-            createObj(data);
-        })
-    searchForm.reset();
-    console.log('clicked')
-    }
-    )
-});
+// Load Bitcoin data by default 
 
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
+    .then(resp => resp.json())
+    .then(data => createObj(data))
+})
+
+// Search Event Listener
+
+document.querySelector('button[type="submit"]').addEventListener('click', (e) => {
+    e.preventDefault();
+    let inputVal = document.querySelector('#searchBar').value;
+    let searchForm = document.getElementById('searchForm')
+    fetch(`https://api.coingecko.com/api/v3/coins/${inputVal}`)
+    .then(resp => resp.json())
+    .then(data => {
+        createObj(data);
+    })
+    searchForm.reset();    
+}
+)
+
+// Helper Functions 
+
+// Create Objects GET request
 
 function createObj(data) {
 
@@ -24,32 +34,45 @@ function createObj(data) {
     let social = {};
     let dev = {};
 
+    let dataList = [market, social, dev]
+
     // Populate Market Object
 
-    market['Name'] = data.name;
-    market['Price'] = data.market_data.current_price.usd;
-    market['Market Cap'] = data.market_data.market_cap.usd;
-    market['Total Volume'] = data.market_data.total_volume.usd;
-    market['24hr Price Change'] = data.market_data.price_change_24h_in_currency.usd;
-    market['WoW Price Change'] = data.market_data.price_change_percentage_7d;
-    market['Market Cap Rank'] = data.market_cap_rank;
+    market['name'] = data.name;
+    market['price'] = data.market_data.current_price.usd;
+    market['market'] = data.market_data.market_cap.usd;
+    market['volume'] = data.market_data.total_volume.usd;
+    market['dod'] = data.market_data.price_change_24h_in_currency.usd;
+    market['wow'] = data.market_data.price_change_percentage_7d;
+    market['rank'] = data.market_cap_rank;
 
     // Populate Social Object
 
-    social['Twitter Followers'] = data.community_data.twitter_followers;
-    social['Reddit Followers'] = data.community_data.reddit_subscribers;
-    social['Average 48hr Reddit Posts'] = data.community_data.reddit_average_posts_48h;
-    social['Reddit 48hr Active Accounts'] = data.community_data.reddit_accounts_active_48h;
+    social['twitter'] = data.community_data.twitter_followers;
+    social['reddit'] = data.community_data.reddit_subscribers;
+    social['reddit-active-posts'] = data.community_data.reddit_average_posts_48h;
+    social['reddit-active-accounts'] = data.community_data.reddit_accounts_active_48h;
+    
 
     // Populate Dev Object
 
-    dev['Subscribers'] = data.developer_data.subscribers;
-    dev['Forks'] = data.developer_data.forks;
-    dev['Stars'] = data.developer_data.stars;
-    dev['4 Week Commits'] = data.developer_data.commit_count_4_weeks;
+    dev['subs'] = data.developer_data.subscribers;
+    dev['forks'] = data.developer_data.forks;
+    dev['stars'] = data.developer_data.stars;
+    dev['commits'] = data.developer_data.commit_count_4_weeks;
 
-    console.log(market)
-    console.log(social)
-    console.log(dev)
+    dataList.forEach(renderObj)
+}
 
+// Render Objects
+
+function renderObj(obj) {
+
+
+    Object.keys(obj).forEach(key => {
+        
+        let cell = document.getElementById(`${key}`);
+        cell.textContent = obj[key];
+    })
+    
 }
